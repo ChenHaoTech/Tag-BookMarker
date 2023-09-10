eb9a95849729d62bec0cab9d9921968099b86a8f
陈浩 <32390273+chenhaoaixuexi@users.noreply.github.com>
2023-09-10 15:21:55 +0800

    M lib/main.dart  M pubspec.lock  M pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:chrome_extension/bookmarks.dart';
 import 'package:flutter/material.dart';
 
 import 'qr_view.dart';
 
-void main() => runApp(const MyApp());
+List<BookmarkTreeNode> list = [];
+void main() {
+  chrome.bookmarks.getTree().then((value) => list.addAll(value));
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -15,7 +20,28 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const QRView(),
+      home: BookMarkView(),
+    );
+  }
+}
+
+
+// 从 bookmark 里面读取数据, 然后渲染出列表 取头三个
+class BookMarkView extends StatelessWidget {
+  BookMarkView(){}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(list.length.toString()),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: list.map((e) => ListTile(title: Text(e.title))).toList(),
+        ),
+      ),
     );
   }
 }
